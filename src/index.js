70fb27e40a93d9481d6f45bb5abfaa036387be0d
Arslonbek <47861284+Arslonbek25@users.noreply.github.com>
2022-03-23 05:40:35 +0500

    basic todo app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { todos: ["hi", "hello"], value: "" };
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    addItem(e) {
+        e.preventDefault();
+        if (!this.state.value || this.state.todos.includes(this.state.value))
+            return;
+        this.setState({
+            todos: [...this.state.todos, this.state.value],
+            value: "",
+        });
+        document.querySelector("form").reset();
+    }
+
+    handleChange(e) {
+        if (e.target.value.length) this.setState({ value: e.target.value });
+    }
+
+    removeItem(index) {
+        this.setState({
+            todos: [...this.state.todos].filter((todo, i) => index !== i),
+        });
+    }
+
+    render() {
+        let list = this.state.todos.map((item, index) => (
+            <li key={index}>
+                {item}
+                <button onClick={() => this.removeItem(index)}>
+                    Remove {index}
+                </button>
+            </li>
+        ));
+
+        return (
+            <>
+                <form onSubmit={(e) => this.addItem(e)}>
+                    <input
+                        type="text"
+                        onChange={(e) => this.handleChange(e)}
+                        placeholder="Enter an item"
+                    />
+                    <button>Add</button>
+                </form>
+                <ul>{list}</ul>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
